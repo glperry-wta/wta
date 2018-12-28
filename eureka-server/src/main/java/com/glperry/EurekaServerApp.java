b9a7c9f1ff3709777940c67a5cd0091742386939
xuzhongpei <xuzhongpei@egowork.com>
2018-12-28 17:52:31 +0800

    first commit

---
+++ eureka-server/src/main/java/com/glperry/EurekaServerApp.java
@@ -0,0 +1,21 @@
+package com.glperry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import javax.swing.*;
+
+/**
+ * @Author: XuZhongPei
+ * @Date: 2018-12-28
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApp.class,args);
+    }
+
+}
---
+++ eureka-server/src/test/java/com/glperry/EurekaServerAppTest.java
@@ -0,0 +1,21 @@
+package com.glperry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author: XuZhongPei
+ * @Date: 2018-12-28
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServerAppTest {
+
+    @Test
+    public void contextLoads(){
+
+    }
+
+}
